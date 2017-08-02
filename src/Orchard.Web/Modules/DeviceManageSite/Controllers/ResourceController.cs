7fedd0f1d440fdbf9f9301736eb651e2394431b6
Gulu <Gulu@Home>
2017-08-03 00:05:18 +0800

    添加资源功能

--- src/Orchard.Web/Modules/DeviceManageSite/Controllers/ResourceController.cs
+++ src/Orchard.Web/Modules/DeviceManageSite/Controllers/ResourceController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Orchard;
+using Orchard.Localization;
+using Orchard.Themes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,12 +11,31 @@ namespace DeviceManageSite.Controllers
 {
     public class ResourceController : Controller
     {
+
+        public ResourceController(
+            IOrchardServices orchardService
+            )
+        {
+            OrchardService = orchardService;
+            T = NullLocalizer.Instance;
+        }
+
+        public IOrchardServices OrchardService { get; set; }
+        public Localizer T { get; set; }
+
         // GET: Resource
         public ActionResult Index()
         {
             return View();
         }
 
+        [Themed]
+        public ActionResult Add()
+        {
+            if (!OrchardService.Authorizer.Authorize(Permissions.ResourceBasic, T("需要更高权限添加资源")))
+                return new HttpUnauthorizedResult();
+            return View();
+        }
 
     }
 }
---
+++ src/Orchard.Web/Modules/DeviceManageSite/Scripts/resourceManage.js
@@ -0,0 +1,27 @@
+﻿/// <reference path="knockout-3.4.2.js" />
+
+$(function () {
+    (function(settings){
+
+        function resouceAddView() {
+            var self = this;
+            self.ipSelected = ko.observable(true);
+            self.telSelected = ko.observable(false);
+        }
+
+        var viewmodel = new resouceAddView();
+
+        ko.applyBindings(viewmodel);
+
+        $("#ipadd").click(function () {
+            viewmodel.ipSelected(true);
+            viewmodel.telSelected(false);
+        });
+
+        $("#teladd").click(function () {
+            viewmodel.ipSelected(false);
+            viewmodel.telSelected(true);
+        })
+
+    })(window.resourceSettings)
+})

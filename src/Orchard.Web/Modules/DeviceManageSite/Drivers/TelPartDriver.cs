6df41fdf95483b91d03f6b14e2646f364040d87a
gulu <gulu@hsgs>
2017-08-04 17:41:55 +0800

    电话资源添加功能

---
+++ src/Orchard.Web/Modules/DeviceManageSite/Drivers/TelPartDriver.cs
@@ -0,0 +1,54 @@
+﻿using DeviceManageSite.Models;
+using Orchard.ContentManagement.Drivers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.ContentManagement;
+using DeviceManageSite.Services;
+using Orchard;
+using Orchard.Localization;
+using Orchard.UI.Notify;
+
+namespace DeviceManageSite.Drivers
+{
+    public class TelPartDriver:ContentPartDriver<TelPart>
+    {
+        private readonly IResourceManageService _resourceManageService;
+        public TelPartDriver(
+            IResourceManageService resourceManageService,
+            IOrchardServices orchardService
+            )
+        {
+            _resourceManageService = resourceManageService;
+            OrchardService = orchardService;
+            T = NullLocalizer.Instance;
+        }
+
+        public IOrchardServices OrchardService { get; set; }
+        public Localizer T { get; set; }
+
+        protected override DriverResult Editor(TelPart part, dynamic shapeHelper)
+        {
+            return ContentShape("Parts_TelPart_Edit",
+                () => shapeHelper.EditorTemplate(TemplateName:"Parts/TelPart.Edit",Model:part,Prefix:Prefix)
+                );
+        }
+
+        protected override DriverResult Editor(TelPart part, IUpdateModel updater, dynamic shapeHelper)
+        {
+            updater.TryUpdateModel(part, Prefix, null, null);
+            var typeRecord = _resourceManageService.GetResTypeByName(part.ResourceType);
+            if(typeRecord == null)
+            {
+                updater.AddModelError("resType", T("当前添加的资源类型不存在"));
+                return Editor(part, shapeHelper);
+            }
+
+            _resourceManageService.NewResource(part.Tel, typeRecord);
+            OrchardService.Notifier.Information(T("添加成功"));
+
+            return Editor(part, shapeHelper);
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/DeviceManageSite/Models/TelPart.cs
@@ -0,0 +1,30 @@
+﻿using DeviceManageSite.Services;
+using Orchard.ContentManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeviceManageSite.Models
+{
+    public class TelPart : ContentPart, IDeviceResource
+    {
+        public ContentPart Part
+        {
+            get
+            {
+                return this;
+            }
+        }
+
+        public string ResourceType
+        {
+            get
+            {
+                return "电话";
+            }
+        }
+
+        public string Tel { get; set; }
+    }
+}

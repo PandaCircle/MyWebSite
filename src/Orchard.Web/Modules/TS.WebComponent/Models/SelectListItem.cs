9de15497a821e701b66b70cf1629aad27223282f
Hsgs <Hsgs@HSGS-PC>
2017-11-13 09:47:15 +0800

    选择列表

--- src/Orchard.Web/Modules/TS.WebComponent/Models/SelectListItem.cs
+++ src/Orchard.Web/Modules/TS.WebComponent/Models/SelectListItem.cs
@@ -8,6 +8,7 @@ namespace TS.WebComponent.Models
     public class SelectListItem
     {
         public string Title { get; set; }
+        public string DisplayText { get; set; }
         public object Data { get; set; }
     }
 }
